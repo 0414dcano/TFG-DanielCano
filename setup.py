76a01556b8709e8596ba41ccd253f428a117baee
0414dcano <100593419+0414dcano@users.noreply.github.com>
2025-05-30 09:37:55 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,34 +3,29 @@ import sys
 import shutil
 import os
 
+
 def has_nvidia_gpu():
     return shutil.which("nvidia-smi") is not None
 
+
 def install_torch():
-    index_url = None
     if has_nvidia_gpu():
         print("✅ NVIDIA GPU detectada. Instalando PyTorch con soporte CUDA...")
-        torch_version = "torch==2.7.0"
-        torchvision_version = "torchvision==0.22.0"
-        
+        torch_version = "torch==2.2.2"
+        torchvision_version = "torchvision==0.17.2"
+        index_url = "https://download.pytorch.org/whl/cu118"
     else:
         print("⚠️ No se detectó GPU NVIDIA. Instalando PyTorch para CPU...")
-        torch_version = "torch==2.7.0+cpu"
-        torchvision_version = "torchvision==0.22.0+cpu"
+        torch_version = "torch==2.2.2"
+        torchvision_version = "torchvision==0.17.2"
         index_url = "https://download.pytorch.org/whl/cpu"
-    
-    if index_url is None:
-        subprocess.check_call([
-        sys.executable, "-m", "pip", "install",
-        torch_version, torchvision_version,
-        ])
-       
-    else:
-        subprocess.check_call([
+
+    subprocess.check_call([
         sys.executable, "-m", "pip", "install",
         torch_version, torchvision_version,
         "--index-url", index_url
-    	])
+    ])
+
 
 def install_requirements():
     requirements_file = "./TFG/requirements.txt"
@@ -41,12 +36,15 @@ def install_requirements():
     print(f"📦 Instalando dependencias desde {requirements_file}...")
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", requirements_file])
 
+
 def ajuste_BDD():
-    subprocess.check_call([sys.executable, "python", "./TFG/manage.py", "makemigrations"])
-    subprocess.check_call([sys.executable, "python", "./TFG/manage.py", "migrate"])
+    subprocess.check_call([sys.executable, "./TFG/manage.py", "makemigrations"])
+    subprocess.check_call([sys.executable, "./TFG/manage.py", "migrate"])
+
 
 def creacion_modelo():
-    subprocess.check_call([sys.executable, "python", "./TFG/entrenar_guardar_modelo.py"])
+    subprocess.check_call([sys.executable, "./TFG/entrenar_guardar_modelo.py"])
+
 
 if __name__ == "__main__":
     install_torch()
@@ -56,4 +54,4 @@ if __name__ == "__main__":
     print("✅ BBDD ajustada correctamente. Se va a proceder a crear el modelo necesario")
     creacion_modelo()
     print("✅ Modelo Creado. Iniciando servidor local")
-    subprocess.check_call([sys.executable, "python", "./TFG/manage.py", "runserver"])
+    subprocess.check_call([sys.executable, "./TFG/manage.py", "runserver"])
